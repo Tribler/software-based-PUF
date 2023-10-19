8fa464ea668290c72435c829b125a53a2d9bc84c
myndcryme <51213021+myndcryme@users.noreply.github.com>
2023-10-19 01:22:32 +0000

    files for save/restore of user device selection at runtime

---
+++ Source Code/puf_xtra/user.py
@@ -0,0 +1,32 @@
+# File for storing user selected device settings.
+# This file is a part of software-based-PUF,
+# https://github.com/Tribler/software-based-PUF
+# Copyright (C) 2023 myndcryme
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##################################################################
+# Configuration settings in this file are set at runtime         #
+# DO NOT set these values manually since they may be overwritten #
+##################################################################
+
+# saved user menu selection
+selected_device = ''
+selected_vidpid = ''
+selected_uid = ''
+
+# for information display
+selected_manufacturer = ''
+selected_product = ''
+selected_hwid = ''
