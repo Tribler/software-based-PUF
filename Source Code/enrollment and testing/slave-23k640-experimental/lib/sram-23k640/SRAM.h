e59aff5c88e328da8cd8f666e0dbc7b435d15f24
myndcryme <51213021+myndcryme@users.noreply.github.com>
2025-02-04 04:30:30 +0000

    Refactored to near original.  Necessary variable reassignments for 23K640 are now handled by derived class XSRAM.

--- Source Code/enrollment and testing/slave-23k640-experimental/lib/sram-23k640/SRAM.h
+++ Source Code/enrollment and testing/slave-23k640-experimental/lib/sram-23k640/SRAM.h
@@ -28,19 +28,13 @@
 #include "Arduino.h"
 #include <SPI.h>
 
-#define SRAM_23K640   // if using 23K640, otherwise defaults to 23LC1024 specs
-
 using namespace std;
 
 class SRAM {
 protected:    // changed from private so that members are visible to XSRAM
-#ifdef SRAM_23K640
-  uint32_t maxram = 32768;    // bytes in 23K640 SRAM
-  uint32_t maxpage = 1024;    // pages in 23K640 SRAM
-#else
   uint32_t maxram = 131072;   // bytes in 23LC1024 SRAM (default)
   uint32_t maxpage = 4096;    // pages in 23LC1024 SRAM (default)
-#endif
+
   uint8_t pin_cs;
   uint8_t pin_hold;
   uint8_t pin_power;
